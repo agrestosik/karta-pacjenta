1ea4fbe528efdbad2636dfc3bb667bdae1696e2e
agrestosik <pduleba@wp.pl>
2025-05-08 18:00:17 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>KARTA PACJENTA</h1>
+      <p>Formularz w przygotowaniu...</p>
+    </div>
+  );
+}
+export default App;
